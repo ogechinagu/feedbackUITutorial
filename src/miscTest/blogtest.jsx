564f93dd3f996abb58f2f9c271b3f18bc99f23e7
Ogechinagu <mzogechee@gmail.com>
2022-10-21 08:30:57 +0000

    Created the Header component

---
+++ src/miscTest/blogtest.jsx
@@ -0,0 +1,40 @@
+
+
+function BlogTest() {
+  const title = "Blog Post";
+
+  const body = "This is my blog post";
+
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+    </div>
+  );
+}
+
+export default BlogTest;
